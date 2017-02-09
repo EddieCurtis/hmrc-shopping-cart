622a57be45eee995d53a5e3facffbe2a78e787e1
Eddie Curtis <e.j.curtis1@gmail.com>
2017-02-08 19:40:19 -0500

    Implement Step 1 - Total cost of list of items.

--- src/Checkout.java
+++ src/Checkout.java
@@ -1,5 +1,8 @@
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Created by eddie on 2/8/17.
@@ -7,8 +10,20 @@ import java.util.Collection;
 public class Checkout {
 
     public static BigDecimal total(Collection<Item> items) {
-        // TODO: implement solution
-        return new BigDecimal("-1");
+
+        BigDecimal total = BigDecimal.ZERO;
+
+        // Group by item identity and store the count of each item.
+        Map<Item, Long> groupedMap = items.stream().collect(
+                Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        // Multiply the count of each item by its cost.
+        for (Map.Entry<Item, Long> entry : groupedMap.entrySet()) {
+            BigDecimal totalForItem = BigDecimal.valueOf(entry.getValue()).multiply(entry.getKey().cost);
+            total = total.add(totalForItem);
+        }
+
+        return total;
     }
 
 }
